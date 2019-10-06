d7741992bf19af8e48ffa7731daf96f6672974e2
kevnoli <kevinols08@gmail.com>
2019-10-06 10:45:14 -0300

    Código e teste em js

---
+++ QuestaoUm/CurtoCircuito.js
@@ -0,0 +1,15 @@
+console.log('Bitwise OU');
+let x = 0;
+for(let i = 0; i < 5; i++){
+ if((1==1) | (x++>3)){
+  console.log('x = ' + x);
+ }
+}
+console.log('\nOU');
+x = 0;
+for(let i = 0; i < 5; i++){
+ if((1==1) || (x++>3)){
+  console.log('x = ' + x);
+ }
+}
+
