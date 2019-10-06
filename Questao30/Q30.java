e9b79ce7bb4d166d3ebca9d27e213289d275a2bb
kevnoli <kevinols08@gmail.com>
2019-10-06 17:00:01 -0300

    Questão 30

---
+++ Questao30/Q30.java
@@ -0,0 +1,13 @@
+public class Q30{
+ public static void main (String[]args){
+  int k,j,l;
+  if((k==1) || (k==2))
+   j = 2 + k - l;
+  if((k==3) || (k==5))
+   j = 3 * k + l;
+  if(k==4)
+   j = 4 * k - 1;
+  if((k==6) || (k==7) || (k==8))
+   j = k - 2;
+ }
+}
